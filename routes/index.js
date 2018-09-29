8b6cdc4f598a83d01261138b7406b482eaff6b05
David Nelson Murray <linearsix@gmail.com>
2018-09-29 14:10:24 -0500

    basic homepage layout complete

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,34 @@ const express = require('express');
 const router = express.Router();
 // const knex = require('../db/knex');
 
+    let show = [{
+        date: "01/23/2019",
+        time: "9:00pm",
+        ven_name: "CBGB's",
+        city: "New York",
+        state: "New York",
+        ticket_link: "NA"
+    }, 
+    {
+        date: "01/23/2019",
+        time: "9:00pm",
+        ven_name: "CBGB's",
+        city: "New York",
+        state: "New York",
+        ticket_link: "NA"
+    },
+    {
+        date: "01/23/2019",
+        time: "9:00pm",
+        ven_name: "CBGB's",
+        city: "New York",
+        state: "New York",
+        ticket_link: "NA"
+    }];
 // render the home page
 router.get('/', (req, res, next) => {
   
-    res.render('index')
+    res.render('index', { show });
 
 });
 
