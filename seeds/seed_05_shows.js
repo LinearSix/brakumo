10a3565e37086cb7c8640fb12f92c929166c9ac8
David Nelson Murray <linearsix@gmail.com>
2018-09-30 20:42:17 -0500

    added shows page and styling

--- seeds/seed_05_shows.js
+++ seeds/seed_05_shows.js
@@ -10,22 +10,29 @@ exports.seed = function(knex, Promise) {
           "show_date": "2018-08-15",
           "show_time": "12:29:45",
           "show_info": "$100 cover - free parking",
-          "show_ticket_link": "NA",
+          "show_ticket_link": "NA"
         },
         {
           "venue_id": 2,
           "show_date": "2018-08-18",
           "show_time": "12:29:45",
           "show_info": "With special guests Dick Dribbler, Poopshoot, and Metallica",
-          "show_ticket_link": "NA",
+          "show_ticket_link": "NA"
         },
         {
           "venue_id": 3,
-          "show_date": "2018-08-15",
+          "show_date": "2018-08-16",
           "show_time": "12:29:45",
           "show_info": "Album release party with free peppermint string cheese and okra buffet",
-          "show_ticket_link": "https://www.ticketmaster.com/",
+          "show_ticket_link": "https://www.ticketmaster.com/"
         },
+        {
+          "venue_id": 2,
+          "show_date": "2018-09-18",
+          "show_time": "13:29:45",
+          "show_info": "With special guests Papa Hemmingway, Foolproof, and Radiohead",
+          "show_ticket_link": "https://www.ticketmaster.com/"
+        }
       ]);
     });
 };
